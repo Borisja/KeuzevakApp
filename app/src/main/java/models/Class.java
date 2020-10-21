3fd6d86ea7ebd61d7ebb8b7963e01ca75e314c7a
Borisja <s1098025@student.hsleiden.nl>
2020-10-21 16:05:04 +0200

    Added user and class models

---
+++ app/src/main/java/models/Class.java
@@ -0,0 +1,10 @@
+package models;
+
+public class Class {
+    public String code;
+    public String name;
+    public String description;
+    public int ec;
+    public int year;
+    public int periode;
+}
